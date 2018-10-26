bc8368f49d905db5ed864d8f34579da2acf03d18
uyuutosa <yu@originalstitch.com>
2018-10-26 15:10:39 +0900

    first commit

---
+++ encoder_decoder.py
@@ -0,0 +1,55 @@
+import json
+import io
+import PIL.Image as I
+import base64
+import zlib
+
+class EncoderDecoder:
+    def __init__(self):
+        pass
+
+    def encode(self, x):
+        return x
+
+    def decode(self, x):
+        return x
+
+class ImageEncoderDecoder(EncoderDecoder):
+    def __init__(self, image_format="jpeg"):
+        super(ImageEncoderDecoder).__init__()
+        self.image_format = image_format
+
+    def encode(self, x):
+        if isinstance(x, str):
+            x = zlib.compress(open(x, 'rb').read())
+            x = base64.b64encode(x)
+        elif isinstance(x, I.Image):
+            img_bytes = io.BytesIO()
+            x.save(img_bytes, format=self.image_format)
+            img_bytes.seek(0)
+            x = zlib.compress(img_bytes.read())
+            x = base64.b64encode(x)
+        return x
+
+    def decode(self, x):
+        return I.open(io.BytesIO(zlib.decompress(base64.b64decode(x))),)
+
+class CSVEncoderDecoder(EncoderDecoder):
+    def __init__(self, ):
+        super(CSVEncoderDecoder).__init__()
+
+    def encode(self, x):
+        if isinstance(x, pd.DataFrame):
+            x = zlib.compress(sizing_result.to_csv().encode())
+
+        elif isinstance(x, str):
+            x = zlib.compress(x.encode())
+        x = base64.b64encode(x)
+        return x
+
+    def decode(self, x):
+        if isinstance(x, str):
+            x = x.encode()
+        x = zlib.decompress(base64.b64decode(x)).decode()
+        x = pd.read_csv(io.StringIO(x),index_col=0)
+        return x
